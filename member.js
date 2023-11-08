40d6bd9ac0427ede0964643d84af705996cb0014
evo-rjbeltran <150205799+evo-rjbeltran@users.noreply.github.com>
2023-11-08 03:31:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+}
+
